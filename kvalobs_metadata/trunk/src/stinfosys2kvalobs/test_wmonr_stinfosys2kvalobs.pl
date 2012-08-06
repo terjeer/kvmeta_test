dedd6b4732821b68f5c7fee43f9d524249ff1b9b
terjeer <terjeer@422d51e2-5941-41ac-8e65-d5b5931239be>
2012-08-06 13:25:22 +0000

    no longer in use

--- kvalobs_metadata/trunk/src/stinfosys2kvalobs/test_wmonr_stinfosys2kvalobs.pl
+++
@@ -1,7 +0,0 @@
-use wmonr_stinfosys2kvalobs;
-
-foreach  my $stationid ( keys %stationid_fromtime ){
-  foreach  my $fromtime ( keys %{$stationid_fromtime{$stationid}} ){
-     print "$stationid $fromtime= $stationid_fromtime{$stationid}{$fromtime}\n";
-   }
-}
